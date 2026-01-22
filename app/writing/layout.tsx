2957893d3c2f08145a853094063df3bd2a643447
Junaid Anjum <junaidanjum@live.com>
2026-01-22 21:02:05 +0530

    feat: wrap article content in AnimatedElement for enhanced animations

--- app/writing/layout.tsx
+++ app/writing/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AnimatedElement } from "@/components/AnimatedElement";
 import { Header } from "@/components/Header";
 import { AsideNavigation, MainNavigation } from "@/components/Navigation";
 
@@ -14,9 +15,11 @@ export default function LayoutBlogPost({
       <AsideNavigation />
       <main>
         <MainNavigation />
-        <article className="prose mt-16 dark:prose-invert text-sm">
-          {children}
-        </article>
+        <AnimatedElement delay={1.3}>
+          <article className="prose mt-16 dark:prose-invert text-sm">
+            {children}
+          </article>
+        </AnimatedElement>
       </main>
       <footer className="mt-23 mb-0"></footer>
     </>
